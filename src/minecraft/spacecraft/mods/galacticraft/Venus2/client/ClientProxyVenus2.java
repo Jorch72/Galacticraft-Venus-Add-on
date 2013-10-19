7e328a014673c99c4359909bfc7224f404b133c9
Matt Albrecht <mattparks5855@gmail.com>
2013-10-19 16:17:54 -0500

    Disabled capes

--- src/minecraft/spacecraft/mods/galacticraft/Venus2/client/ClientProxyVenus2.java
+++ src/minecraft/spacecraft/mods/galacticraft/Venus2/client/ClientProxyVenus2.java
@@ -83,34 +83,34 @@ public class ClientProxyVenus2 extends CommonProxyVenus2
         NetworkRegistry.instance().registerChannel(new ClientPacketHandler(), GCVenus2.CHANNEL, Side.CLIENT);
         ClientProxyVenus2.eggRenderID = RenderingRegistry.getNextAvailableRenderId();
         RenderingRegistry.registerBlockHandler(new GCVenus2BlockRendererTreasureChest(ClientProxyVenus2.treasureRenderID));
-
-        try
-        {
-            int timeout = 10000;
-            URL capeListUrl = new URL("https://raw.github.com/Super4Ever4MC/Galacticraft-Venus-Add-ont/master/capes.txt");
-            URLConnection connection = capeListUrl.openConnection();
-            connection.setConnectTimeout(timeout);
-            connection.setReadTimeout(timeout);
-            InputStream stream = connection.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-            
-            String line;
-            while ((line = reader.readLine()) != null)
-            {
-                if ((line.contains(":")))
-                {
-                    int splitLocation = line.indexOf(":");
-                    String username = line.substring(0, splitLocation);
-                    String capeUrl = "https://raw.github.com/Super4Ever4MC/Galacticraft-Venus-Add-on/master/capes/" + line.substring(splitLocation + 1) + ".png";
-                    ClientProxyVenus2.capeMap.put(username, capeUrl);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            FMLLog.severe("Error while setting up Venus donor capes");
-            e.printStackTrace();
-        }
+    }
+//        try
+//        {
+//            int timeout = 10000;
+//            URL capeListUrl = new URL("https://raw.github.com/Super4Ever4MC/Galacticraft-Venus-Add-ont/master/capes.txt");
+//            URLConnection connection = capeListUrl.openConnection();
+//            connection.setConnectTimeout(timeout);
+//            connection.setReadTimeout(timeout);
+//            InputStream stream = connection.getInputStream();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+//            
+//            String line;
+//            while ((line = reader.readLine()) != null)
+//            {
+//                if ((line.contains(":")))
+//                {
+//                    int splitLocation = line.indexOf(":");
+//                    String username = line.substring(0, splitLocation);
+//                    String capeUrl = "https://raw.github.com/Super4Ever4MC/Galacticraft-Venus-Add-on/master/capes/" + line.substring(splitLocation + 1) + ".png";
+//                    ClientProxyVenus2.capeMap.put(username, capeUrl);
+//                }
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//            FMLLog.severe("Error while setting up Venus donor capes");
+//            e.printStackTrace();
+//        }
      
     @Override
     public void registerRenderInformation()
