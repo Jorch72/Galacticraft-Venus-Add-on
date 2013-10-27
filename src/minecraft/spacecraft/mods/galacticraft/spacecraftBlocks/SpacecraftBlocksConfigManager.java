73283cc49d56dcf8f76ce34ee2a7bba3352f397d
Matt Albrecht <mattparks5855@gmail.com>
2013-10-27 17:58:37 -0500

    Finish Config File

--- src/minecraft/spacecraft/mods/galacticraft/spacecraftBlocks/SpacecraftBlocksConfigManager.java
+++ src/minecraft/spacecraft/mods/galacticraft/spacecraftBlocks/SpacecraftBlocksConfigManager.java
@@ -87,8 +87,46 @@ public class SpacecraftBlocksConfigManager
         {
             SpacecraftBlocksConfigManager.configuration.load();
 
-//	        SpacecraftBlocksConfigManager.VenusGrassID = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Grass ID", 7004).getInt(7004);	        
+//	        SpacecraftBlocksConfigManager.idVenusGrass = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Grass ID", 7004).getInt(7004);	        
 
+            SpacecraftBlocksConfigManager.idVenusGrass = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Grass ID", 7004).getInt(7004);
+            SpacecraftBlocksConfigManager.idVenusDirt = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Dirt ID", 7005).getInt(7005);
+            SpacecraftBlocksConfigManager.idVenusStone = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Stone ID", 7006).getInt(7006);
+            SpacecraftBlocksConfigManager.idVenusCobblestone = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Cobblestone ID", 7007).getInt(7007);
+            SpacecraftBlocksConfigManager.idVenusRedGemBlock = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Gem Block ID", 7008).getInt(7008);
+            SpacecraftBlocksConfigManager.idVenusMeteorBlock = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Meteor Block ID", 7009).getInt(7009);
+            SpacecraftBlocksConfigManager.idVenusBrick = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Brick ID", 7010).getInt(7010);
+            SpacecraftBlocksConfigManager.idVenusSulferBlock = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Sulfer Block ID", 7011).getInt(7011);
+            SpacecraftBlocksConfigManager.idVenusRedGemOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Gem Ore ID", 7012).getInt(7012);
+            SpacecraftBlocksConfigManager.idVenusMeteorOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Meteor Ore ID", 7013).getInt(7013);
+            SpacecraftBlocksConfigManager.idVenusSulferOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Sulfer Ore ID", 7014).getInt(7014);
+            SpacecraftBlocksConfigManager.idVenusIronOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Iron Ore ID", 7015).getInt(7015);
+            SpacecraftBlocksConfigManager.idVenusCoalOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Coal Ore ID", 7016).getInt(7016);
+            SpacecraftBlocksConfigManager.idVenusTinOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Tin Ore ID", 7017).getInt(7017);
+            SpacecraftBlocksConfigManager.idVenusCopperOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Copper Ore ID", 7018).getInt(7018);
+            SpacecraftBlocksConfigManager.idVenusBasalt = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Basalt ID", 7019).getInt(7019);
+            SpacecraftBlocksConfigManager.idVenusHalfBasalt = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Basalt Slab ID", 7020).getInt(7020);
+            SpacecraftBlocksConfigManager.idEvolvedBlazeEgg = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Venus Blaze Egg ID", 7021).getInt(7021);
+            				
+            SpacecraftBlocksConfigManager.idPlutoGrass = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Pluto Grass ID", 7022).getInt(7022);
+            SpacecraftBlocksConfigManager.idPlutoDirt = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Pluto Dirt ID", 7023).getInt(7023);
+            SpacecraftBlocksConfigManager.idPlutoStone = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Pluto Stone ID", 7024).getInt(7024);
+            SpacecraftBlocksConfigManager.idPlutoCobblestone = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Pluto Cobblestone ID", 7025).getInt(7025);
+            SpacecraftBlocksConfigManager.idPlutoBrick = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Pluto Brick ID", 7026).getInt(7026);
+            				
+            SpacecraftBlocksConfigManager.idMercuryGrass = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Grass ID", 7027).getInt(7027);
+            SpacecraftBlocksConfigManager.idMercuryDirt = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Dirt ID", 7028).getInt(7028);
+            SpacecraftBlocksConfigManager.idMercuryStone = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Stone ID", 7029).getInt(7029);
+            SpacecraftBlocksConfigManager.idMercuryCobblestone = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Cobblestone ID", 7030).getInt(7030);
+            SpacecraftBlocksConfigManager.idMercuryBrick = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Brick ID", 7031).getInt(7031);
+            SpacecraftBlocksConfigManager.idMercuryRedGemOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Gem Ore ID", 7032).getInt(7032);
+            SpacecraftBlocksConfigManager.idMercuryCoalOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Coal Ore ID", 7033).getInt(7033);
+            SpacecraftBlocksConfigManager.idMercuryTinOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Tin Ore ID", 7034).getInt(7034);
+            SpacecraftBlocksConfigManager.idMercuryCopperOre = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Mercury Copper Ore ID", 7035).getInt(7035);
+            				
+            SpacecraftBlocksConfigManager.idJupiterLightGas = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Jupiter Light Gas ID", 7036).getInt(7006);
+            SpacecraftBlocksConfigManager.idJupiterMediumGas = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Jupiter Medium Gas ID", 7037).getInt(7037);
+            SpacecraftBlocksConfigManager.idJupiterHeavyGas = SpacecraftBlocksConfigManager.configuration.get("Blocks", "Jupiter Heavy Gas ID", 7038).getInt(7038);    
         }
         catch (final Exception e)
         {
