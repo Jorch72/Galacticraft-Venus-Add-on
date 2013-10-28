2182bc4b66309bd9802d3dace304bee6bc80c8dd
Matt Albrecht <mattparks5855@gmail.com>
2013-10-27 21:03:14 -0500

    Begin Saturn

---
+++ src/minecraft/spacecraft/mods/galacticraft/saturn/util/RecipeUtilJupiter.java
@@ -0,0 +1,9 @@
+package spacecraft.mods.galacticraft.saturn.util;
+
+import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
+import micdoodle8.mods.galacticraft.api.recipe.INasaWorkbenchRecipe;
+import net.minecraft.item.ItemStack;
+
+public class RecipeUtilJupiter
+	{
+}
